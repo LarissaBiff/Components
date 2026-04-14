751ee645ea2b79f3b78a0f400ec260c588fc5a03
LarissaBiff <biff.larissa@gmail.com>
2026-04-14 11:51:49 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script setup>
+  import buttonChild from './components/buttonChild.vue';
+</script>
+
+
+<template>
+  <div class="container">
+      <div>
+        <button class="btn btn-novo">Novo</button>
+        <button class="btn btn-editar">Editar</button>
+        <button class="btn btn-salvar">Salvar</button>
+        <button class="btn btn-cancelar">Cancelar</button>
+        <button class="btn btn-excluir">Excluir</button>
+      </div>
+      <div>
+        <buttonChild :nome="'Novo'" :tipo="'btn'" :acao="'btn-novo'"> </buttonChild>
+      </div>
+  </div>
+</template>
+
+
+<style scoped>
+
+.btn {
+  background-color: rgb(179, 46, 68);
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin: 5px;
+  font-weight: bolder;
+}
+.btn-novo {
+  background-color: white;
+  color: rgb(179, 46, 68);
+}
+.btn-editar {
+  background-color: rgb(179, 77, 46);
+  color:  white;
+}
+.btn-salvar {
+  background-color:  rgb(50, 138, 57); 
+  color: white;
+}
+.btn-cancelar {
+  background-color:  rgb(46, 48, 179);
+  color: white;
+}
+.btn-excluir {
+  background-color: rgb(179, 46, 68);
+  color:  white;
+}
+.btn button {
+  cursor: pointer;
+
+}
+</style>
---
+++ src/components/buttonChild.vue
@@ -0,0 +1,29 @@
+<script setup>
+    defineProps(['nome', 'tipo', 'acao'])
+</script>
+
+
+<template>
+    <button :class="[tipo, acao]">
+        {{ nome }}
+    </button>
+</template>
+
+
+<style scoped>
+.btn {
+  background-color: rgb(179, 46, 68);
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin: 5px;
+  font-weight: bolder;
+}
+
+.btn-novo {
+  background-color: white;
+  color: rgb(179, 46, 68);
+}
+</style>
